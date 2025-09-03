806a5ab4ee5dd89391105e1ce58e6bce90561796
Abhimanyu Moharana <abhim.moharana@gmail.com>
2025-09-04 05:00:00 +0530

    added in memory user details

--- Employee/src/main/java/com/employees/Employee/mysecurity/EmployeeSecurity.java
+++ Employee/src/main/java/com/employees/Employee/mysecurity/EmployeeSecurity.java
@@ -6,6 +6,10 @@ import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AuthorizeHttpRequestsConfigurer;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 import java.net.http.HttpRequest;
@@ -19,9 +23,21 @@ public class EmployeeSecurity
     {
         http.csrf(csrf->csrf.disable());
         http.authorizeHttpRequests((requests) -> ((AuthorizeHttpRequestsConfigurer.AuthorizedUrl)requests.anyRequest()).authenticated());
-        http.formLogin(Customizer.withDefaults());
+        //http.formLogin(Customizer.withDefaults());
         http.httpBasic(Customizer.withDefaults());
         return (SecurityFilterChain)http.build();
+    }
+
+    @Bean
+    UserDetailsService userDetailsService()
+    {
+        UserDetails user1 = User.withUsername("user")
+                .password("{noop}user")
+                .roles("USER").build();
+        UserDetails user2 = User.withUsername("admin")
+                .password("{noop}admin")
+                .roles("ADMIN").build();
+        return  new InMemoryUserDetailsManager(user1,user2);
     }
 
 }
